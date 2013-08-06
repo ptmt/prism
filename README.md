b597e0efce954f640f526f1d3c2942f701574c7f
unknownexception <unknownliveid@hotmail.com>
2013-08-06 22:28:46 +0700

    jenkins status, update for build script

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 prism
 =====
 
+[![Build Status](http://jenkins.phinitive.com/buildStatus/icon?job=PRISM)](http://jenkins.phinitive.com/buildStatus/icon?job=PRISM)
+
 For build make sure you have installed mono 3.2+
 
 or use vagrantup:
--- src/jenkins_deploy.sh
+++ src/jenkins_deploy.sh
@@ -2,7 +2,6 @@
 
 mono /usr/local/lib/mono/4.5/xbuild.exe src/prism.sln
 cp build/* /var/www/prism.phinitive.com/last
-cp src/public /var/www/prism.phinitive.com/last/public
-#kill process
-killall -9 Prism.App 
+killall -9 mono
+mv /var/www/prism.phinitive.com/last/* /var/www/prism.phinitive.com/current/ 
 mono /var/www/prism.phinitive.com/current/public/Prism.App.exe &
