120082e476b311d6e2d07d0f53863a7e153610d2
unknownexception <unknownliveid@hotmail.com>
2013-08-07 22:53:00 +0700

    add nancyfx first preview

--- src/prism.app/App.cs
+++ src/prism.app/App.cs
@@ -1,4 +1,7 @@
-﻿using Owin;
+﻿using Nancy;
+using Nancy.Conventions;
+using Nancy.Owin;
+using Owin;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -22,18 +25,29 @@ namespace Prism.App
 
             app.UseLogger()
                .UseErrorPage()
+              // .UseStaticFiles("public")
                .UseNancy()
-               //.UseWebApi(config)
-                //.UseFileServer(options =>
-                // {
-                //     options.WithRequestPath("").WithPhysicalPath("public");
-                // })
-               .UseStaticFiles("public")
-               .UseSendFileFallback()
+                //.UseWebApi(config)
+             
+               
+               //.UseSendFileFallback()
                .UseDiagnosticsPage("/test.html");
                
             
         }
     }
+    public class CustomBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions conventions)
+        {
+            base.ConfigureConventions(conventions);           
+            
+            conventions.StaticContentsConventions.Add(               
+                StaticContentConventionBuilder.AddDirectory("/", "public")
+            );
+        }
+    }
+
+   
 
 }
--- src/prism.app/Modules/AccountModule.cs
+++ src/prism.app/Modules/AccountModule.cs
@@ -1,4 +1,7 @@
 ﻿using Nancy;
+using OAuth2;
+using OAuth2.Client;
+using Prism.App.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +10,29 @@ using System.Threading.Tasks;
 
 namespace Prism.App.Modules
 {
-    public class AccountModule : NancyModule
+    public class ApiModule : NancyModule
     {
-        public AccountModule()
+        private readonly AuthorizationRoot authorizationRoot;        
+        private readonly FoursquareProcessing foursquareProcessing;
+
+        public ApiModule()
+            : base("/api")
         {
+            this.authorizationRoot = new AuthorizationRoot();
+            this.foursquareProcessing = new FoursquareProcessing();
+
+
             Get["/moo"] = x => "moo";
+
+            Get["/login"] = _ =>
+            {
+                return GetFoursquareClient().GetLoginLinkUri();
+            };
+        }
+
+        private IClient GetFoursquareClient()
+        {
+            return authorizationRoot.Clients.First();
         }
     }
 }
