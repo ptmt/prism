0bafd40582b2108c6f7924acb49be09ca81f9bf5
unknownexception <d.loktev@ftc.ru>
2013-08-19 14:19:44 +0700

    close #13

--- jenkins_job.sh
+++ jenkins_job.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 mono /usr/local/lib/mono/4.5/xbuild.exe src/prism.sln 
-cp build/* /var/www/prism.phinitive.com/last -R
+echo "copying files from build directory to last"
+cp build/* /var/www/prism.phinitive.com/last -R | exit 1
 supervisorctl stop prismmono
-rm /var/www/prism.phinitive.com/current/* -R 
-mv /var/www/prism.phinitive.com/last/* /var/www/prism.phinitive.com/current/ 
+wait
+rm /var/www/prism.phinitive.com/current/* -R | exit 2
+echo "removing old files"
+mv /var/www/prism.phinitive.com/last/* /var/www/prism.phinitive.com/current/ -f
+echo "moving files from last build to current production folder"
 supervisorctl start prismmono
