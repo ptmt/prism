79c21cc2e4e7a8161ad3ee2683d3f5346602ee05
unknownexception <unknownliveid@hotmail.com>
2013-09-24 21:32:58 +0700

    into div

--- src/prism.app/Program.cs
+++ src/prism.app/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Owin.Hosting;
 using Owin;
 using System.Reflection;
 using NowinWebServer;
+using System.Configuration;
 
 namespace Prism.App
 {
@@ -11,25 +12,27 @@ namespace Prism.App
     {
         static void Main(string[] args)
         {            
-            string env = args.Length > 0 ? args[0] : "dev";
+            int port = args.Length > 0 ? Int32.Parse(args[0]) : 8888;
             var options = new StartOptions
             {
                 ServerFactory = "NowinWebServer.OwinServerFactory",               
-                Port = 8888
+                Port = new Nullable<int>(port)
             };
+           
+            //options.Urls.Add("localhost");
+            //options.Urls.Add("prism.phinitive.com");
+            //options.Urls.Add("127.0.0.1");
+            //options.Settings.Add("env", env);
 
-            if (env == "dev")
-            {                
-                options.Urls.Add("devprism.phinitive.com");                
-            }
-            options.Urls.Add("localhost");
-            options.Urls.Add("prism.phinitive.com");
-            options.Urls.Add("127.0.0.1");
-            options.Settings.Add("env", env);
+            //var col = System.Configuration.ConfigurationManager.GetSection("OAuth") as ConfigurationElementCollection;
+            //foreach (ConfigurationElement c in col)
+            //{
+            //    Console.WriteLine(c["clientType"]);
+            //}
 
             using (WebApp.Start<Startup>(options))
             {
-                Console.WriteLine("Running a http server on port 8888");
+                Console.WriteLine("Running a http server on port " + port.ToString());
                 while (true) { }
             }
         }
